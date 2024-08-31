68abf0b42c1b52cad8eefe4c713aeba6683bd98c
Christopher Mejo <cmejo@users.noreply.github.com>
2024-08-31 18:27:31 -0400

    Update data_ingestion.py

--- AI_Scholar_v2/word2vec-knowledge-graph/src/data_ingestion.py
+++ AI_Scholar_v2/word2vec-knowledge-graph/src/data_ingestion.py
@@ -14,6 +14,6 @@ def pdf_to_text(pdf_folder):
     return texts
 
 if __name__ == "__main__":
-    pdf_folder = '../data/pdfs'
+    pdf_folder = '//home/cmejo/arxiv-dataset/pdf'
     texts = pdf_to_text(pdf_folder)
     print(texts)
