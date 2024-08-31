b01f672e0af688804bae210209b2f384ec408ddd
Christopher Mejo <cmejo@users.noreply.github.com>
2024-08-31 18:25:44 -0400

    Create data_ingestion.py

---
+++ AI_Scholar_v2/word2vec-knowledge-graph/src/data_ingestion.py
@@ -0,0 +1,19 @@
+import os
+from PyPDF2 import PdfReader
+
+def pdf_to_text(pdf_folder):
+    texts = []
+    for pdf_file in os.listdir(pdf_folder):
+        if pdf_file.endswith('.pdf'):
+            pdf_path = os.path.join(pdf_folder, pdf_file)
+            reader = PdfReader(pdf_path)
+            text = ''
+            for page in reader.pages:
+                text += page.extract_text()
+            texts.append({'title': pdf_file, 'text': text})
+    return texts
+
+if __name__ == "__main__":
+    pdf_folder = '../data/pdfs'
+    texts = pdf_to_text(pdf_folder)
+    print(texts)
